d86bb3c090c342197559b58c41da7ac6e06af810
Jurgen Mahn <jurgen@mahn.it>
2025-10-03 11:09:11 +0200

    set pcie_aspm to off since its slowing down pcie speed (powersave mode)

--- fix_hp.sh
+++ fix_hp.sh
@@ -8,7 +8,7 @@ IFS=$'\n\t'
 REQUIRED_PKGS=(acpica-tools binwalk)
 GRUB_FILE="/etc/default/grub"
 GRUB_KEY="GRUB_CMDLINE_LINUX_DEFAULT"
-REQUIRED_KERNEL_FLAGS=("mem_sleep_default=deep" "pcie_aspm=force")
+REQUIRED_KERNEL_FLAGS=("mem_sleep_default=deep" "pcie_aspm=off")
 POSTINST_TARGET="/etc/kernel/postinst.d/yy-s3-sleep-fix"
 SYSTEMD_SLEEP_FIX="/lib/systemd/system-sleep/usb_wakeup_fix_s3.sh"
 INITRD="/boot/initrd.img-$(uname -r)"
